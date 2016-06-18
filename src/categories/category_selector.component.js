98ecc83e4ed1891d71999e184b17575be517a2b5
Akixe Otegi <akixe.otegi@gmail.com>
2016-06-18 11:42:21 +0200

    Categorías componentizadas

---
+++ src/categories/category_selector.component.js
@@ -0,0 +1,39 @@
+(function() {
+    angular
+        .module('whatapop')
+        .component('categorySelector', {
+            templateUrl: 'src/categories/category_selector.tmpl.html',
+            controller: CategorySelectorComponent
+        })
+
+        CategorySelectorComponent.$inject = ['$log', 'CategoryService', 'lodash']
+
+        function CategorySelectorComponent($log, CategoryService, _) {
+            var $ctrl = this
+             
+            $ctrl.categories = {
+                items : []
+            }
+            // Interface
+            $ctrl.loadCategories = loadCategories
+            $ctrl.categoryFilterChanged = categoryFilterChanged
+            // Init
+            $ctrl.loadCategories();
+
+            //======IMPL======
+            function loadCategories () {
+                return CategoryService
+                    .getAll()
+                    .then(function (categories) {
+                        $ctrl.categories.items = categories;
+                        _.forEach($ctrl.categories.items, function (item) {
+                            item.selected = true
+                        })
+                    }) 
+            }
+
+            function categoryFilterChanged (category) {
+                category.selected = !category.selected
+            }
+        }
+})()
